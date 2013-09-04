c3e85e86e0e29daab68da6561f1488134b40f6f2
Mikael Gueck <gumi@iki.fi>
2013-09-05 00:07:52 +0300

    puppet master and rundeck

--- devops-infra/puppetmaster/manifests/site.pp
+++ devops-infra/puppetmaster/manifests/site.pp
@@ -1,14 +1,97 @@
+define download_file($site="", $cwd="", $creates="", $require="", $user="") {
 
+  exec { $name:
+    command => "/usr/bin/wget -q ${site}/${name}",
+    cwd => $cwd,
+    creates => "${cwd}/${name}",
+    require => $require,
+    user => $user,
+  }
 
-node admin.mikaelhg.dev {
+}
 
-  package { 'puppetmaster':
+class mikaelhg::rundeck {
+
+  download_file { 'rundeck-1.6.1-1-GA.deb':
+    site => 'http://download.rundeck.org/deb',
+    cwd => '/tmp/',
+    creates => '/tmp/rundeck-1.6.1-1-GA.deb',
+    require => [],
+    user => 'root'
+  }
+
+  package { 'openjdk-7-jdk':
     ensure => latest
   }
 
   package { 'rundeck':
     provider => 'dpkg',
-    source => 'http://download.rundeck.org/deb/rundeck-1.6.1-1-GA.deb'
+    ensure => latest,
+    source => '/tmp/rundeck-1.6.1-1-GA.deb',
+    require => [
+      Package['openjdk-7-jdk'],
+      Download_file['rundeck-1.6.1-1-GA.deb']
+    ]
+  }
+  
+}
+
+class mikaelhg::puppetmaster {
+
+  package { 'puppetmaster':
+    ensure => latest
   }
 
+  service { 'puppetmaster':
+    ensure => running,
+    require => [
+      Package['puppetmaster'],
+      File['/etc/puppet/manifests'],
+      File['/etc/puppet/modules'],
+      File['/etc/puppet/templates'],
+      File['/etc/puppet/autosign.conf']
+    ]
+  }
+
+  file { '/etc/puppet/manifests':
+    ensure => link,
+    force => true,
+    target => '/vagrant/puppetmaster/manifests',
+    require => Package['puppetmaster']
+  }
+  
+  file { '/etc/puppet/modules':
+    ensure => link,
+    force => true,
+    target => '/vagrant/puppetmaster/modules',
+    require => Package['puppetmaster']
+  }
+
+  file { '/etc/puppet/templates':
+    ensure => link,
+    force => true,
+    target => '/vagrant/puppetmaster/templates',
+    require => Package['puppetmaster']
+  }
+  
+  file { '/etc/puppet/autosign.conf':
+    content => '*.mikaelhg.dev'
+  }
+  
+}
+
+node 'admin.mikaelhg.dev' {
+
+  include mikaelhg::rundeck
+  include mikaelhg::puppetmaster
+
+}
+
+node 'cia.mikaelhg.dev' {
+}
+
+node 'app1.mikaelhg.dev' {
+}
+
+node 'app2.mikaelhg.dev' {
 }
